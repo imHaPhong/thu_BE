1b7f90640b3e598aa05895fcb2fcd9194562f044
imHaPhong <64822110+imHaPhong@users.noreply.github.com>
2021-05-06 00:12:53 +0700

    sd

--- controller/Admin.Controller.js
+++ controller/Admin.Controller.js
@@ -11,45 +11,49 @@ module.exports = {
   //     res.status(200).json(user)
   // },
   addNews: async (req, res) => {
-    const news = await News(req.body)
-    await news.save()
-    res.status(200).json(news)
+    const news = await News(req.body);
+    await news.save();
+    res.status(200).json(news);
   },
-  
+
   addScore: async (req, res) => {
-    const score = await Score(req.body)
-    await score.save()
-    res.status(200).json(score)
+    try {
+      const score = await Score(req.body);
+      await score.save();
+      res.status(200).json(score);
+    } catch (error) {
+      res.status(200).json(error);
+    }
   },
   addClub: async (req, res) => {
-    const club = await Club(req.body)
-    await club.save()
-    res.status(200).json(club)
+    const club = await Club(req.body);
+    await club.save();
+    res.status(200).json(club);
   },
   addLeague: async (req, res) => {
-    const isLeague = await League.findOne({name: req.body.name})
-    if(isLeague) {
-      isLeague.clubs = isLeague.clubs.concat(req.body.clubs)
-      await isLeague.save()
-      return res.status(200).json(isLeague)
+    const isLeague = await League.findOne({ name: req.body.name });
+    if (isLeague) {
+      isLeague.clubs = isLeague.clubs.concat(req.body.clubs);
+      await isLeague.save();
+      return res.status(200).json(isLeague);
     }
-    const league = await League(req.body)
-    await league.save()
-    res.status(200).json(league)
+    const league = await League(req.body);
+    await league.save();
+    res.status(200).json(league);
   },
   editNews: async (req, res) => {
     console.log(req.body.id);
-    const newc = await News.findById(req.body.id)
+    const newc = await News.findById(req.body.id);
     console.log(newc);
-    newc.title = req.body.title
-    newc.link = req.body.link
-    await newc.save()
-    res.status(200).json(newc)
+    newc.title = req.body.title;
+    newc.link = req.body.link;
+    await newc.save();
+    res.status(200).json(newc);
   },
   adminDeleteComment: async (req, res) => {
-    const posts = await News.findById(req.body.pId)
-    posts.comment = posts.comment.filter(el => el._id != req.body.cId)
-    await posts.save()
-    res.json(posts)
-  }
+    const posts = await News.findById(req.body.pId);
+    posts.comment = posts.comment.filter((el) => el._id != req.body.cId);
+    await posts.save();
+    res.json(posts);
+  },
 };
